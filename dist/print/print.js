a149062da2b7d0c3fbab4128f19cc6a639c6ad75
LianghuiYin <lianghui_yin@163.com>
2018-02-18 21:19:15 +0800

    remove dist folder

--- dist/print/print.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	var content = window.opener.$(".print-content").clone()
-        .find(".hidden-print").remove().end();
-        $(".print-title").after(content.prop("outerHTML"));
-    document.title = content.find(".panel-title").text() + "--" + document.title;
-});
